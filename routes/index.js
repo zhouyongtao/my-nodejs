2b68aa51c44b068ac08216a5134184fddbea1dae
Irving <zhouyongtao@outlook.com>
2014-05-27 23:52:59 +0800

    测试中文!

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+   console.log("I'am irving! 时间: 2014年5月27日22:57:43 ");
   res.render('index', { title: 'Express' });
 });
 
